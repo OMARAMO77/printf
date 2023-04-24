f288175421d0427b5269c76296b7a51d31b6cb2e
Hotty Frank <hottensiamwihaki@gmail.com>
2023-04-24 14:53:42 +0300

    Header file to all prototypes. Includes some built .h file too

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int _printint(int n);
+int _puts(char *s);
+
+#endif

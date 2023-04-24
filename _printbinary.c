d1b85f429015e4afb5d24ed36302593dbdbfeab2
Hotty Frank <hottensiamwihaki@gmail.com>
2023-04-24 21:48:10 +0300

    _printbinary.c

--- _printbinary.c
+++ _printbinary.c
@@ -1,37 +1,23 @@
 #include "main.h"
-
 /**
- * _printbinary - Prints the binary representation of an unsigned integer.
- * @x: unsigned integer to print in binary.
- *
- * Return: The number of characters printed.
+ * _printbinary - coverts unsigned integer to binary
+ * @x: unsigned interger parameter.
+ * Return: Binary value.
  */
 int _printbinary(unsigned int x)
 {
-	int counter = 0;
-	int bits = 0;
-	unsigned int divisor = 1;
+	unsigned int num;
+	unsigned int modul;
+	unsigned int quontient;
 
-	while ((x / divisor) >= 2)
+	if (x <= 0)
 	{
-		divisor *= 2;
+		return (0);
 	}
+	modul = x % 2;
+	quontient = x / 2;
 
-	while (divisor != 0)
-	{
-		if ((x / divisor) % 2 != 0)
-		{
-			_putchar('1');
-			counter++;
-		}
-		else if (counter > 0 || divisor == 1)
-		{
-			_putchar('0');
-			counter++;
-		}
-
-		divisor /= 2;
-	}
-
-	return (counter);
+	num = _printbinary(quontient);
+	_putchar('0' + modul);
+	return (num + 1);
 }

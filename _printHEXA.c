2749ba99e0a5d5002e7e2247759c7d14e0e9a2c0
Hotty Frank <hottensiamwihaki@gmail.com>
2023-04-25 21:08:45 +0300

    _printHEXA.c

--- _printHEXA.c
+++ _printHEXA.c
@@ -2,32 +2,32 @@
 
 /**
  * _printHEXA - converts an unsigned int into hexadecimal number
- * @X: a decimal number
+ * @H: arguments
  *
  * Return: returns the number of characters printed
  */
-
-int _printHEXA(unsigned int X)
+int _printHEXA(va_list H)
 {
 	char hex[8];
 	int i = 0;
 	int counter = 0;
 	int n;
+	unsigned int X = va_arg(H, unsigned int);
 
-	while (X)
-	{
-		n = X % 16;
-		if (n < 10)
+		while (X)
 		{
-			hex[i] = '0' + n;
+			n = X % 16;
+			if (n < 10)
+			{
+				hex[i] = '0' + n;
+			}
+			else
+			{
+				hex[i] = 'A' + n - 10;
+			}
+			X /= 16;
+			i++;
 		}
-		else
-		{
-			hex[i] = 'A' + n - 10;
-		}
-		X /= 16;
-		i++;
-	}
 	while (i--)
 	{
 		counter += _putchar(hex[i]);

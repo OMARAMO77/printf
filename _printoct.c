b4720c6175f15838b507a926c25ef58f90529240
Omar <omar0642624692@gmail.com>
2023-04-25 10:27:32 -0700

    _printoct.c

--- _printoct.c
+++ _printoct.c
@@ -1,18 +1,19 @@
 #include "main.h"
 
 /**
- * _printoct - converts an unsigned int into hexadecimal number
- * @o: a decimal number
- *
- * Return: returns the number of characters printed
- */
+* _printoct - converts an unsigned int into hexadecimal number
+* @o: a decimal number
+*
+* Return: returns the number of characters printed
+*/
 
-int _printoct(unsigned int o)
+int _printoct(va_list H)
 {
 	char oct[12];
 	int i = 0;
 	int counter;
 	int n;
+	unsigned int o = va_arg(H, unsigned int);
 
 	while (o)
 	{

88c3e2e6fb1bf3fcc5144b4da70365c5dff37abb
Omar <omar0642624692@gmail.com>
2023-04-25 05:19:09 -0700

    main.h

--- main.h
+++ main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#define BUFFER_SIZE 1024
+
 #include <stdio.h>
 #include <stdarg.h>
 #include <string.h>
@@ -11,5 +13,9 @@ int _printf(const char *format, ...);
 int _printint(int n);
 int _puts(char *s);
 int _printbinary(unsigned int x);
+int _printhex(unsigned int x);
+int _printHEXA(unsigned int X);
+int _printoct(unsigned int o);
+int _printuns(unsigned int u);
 
 #endif

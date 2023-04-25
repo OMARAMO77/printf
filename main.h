466816fc98eac90cd6bdfa8bcbc7237b04323e19
Hotty Frank <hottensiamwihaki@gmail.com>
2023-04-25 21:33:09 +0300

    main.h

--- main.h
+++ main.h
@@ -3,19 +3,35 @@
 
 #define BUFFER_SIZE 1024
 
+/**
+ * struct format - match the conversion specifiers for the printf function
+ * @id: type char pointer of the specifier
+ * @f: type pointer to function for the conversion specifier
+ */
+
+typedef struct format
+{
+	char *id;
+	int (*f)();
+} convert_match;
+
+
 #include <stdio.h>
 #include <stdarg.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
+
 
 int _putchar(char c);
 int _printf(const char *format, ...);
-int _printint(int n);
-int _puts(char *s);
-int _printbinary(unsigned int x);
-int _printhex(unsigned int x);
-int _printHEXA(unsigned int X);
-int _printoct(unsigned int o);
-int _printuns(unsigned int u);
-
+int _printint(va_list H);
+int _puts(va_list H);
+int _printbinary(va_list H);
+int _printhex(va_list H);
+int _printHEXA(va_list H);
+int _printoct(va_list H);
+int _printuns(va_list H);
+int _printchar(va_list H);
+int _printpercent(void);
 #endif

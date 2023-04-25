658e98eb68eab84e76a82753da5d1d74e90bde5c
Hotty Frank <hottensiamwihaki@gmail.com>
2023-04-25 22:43:50 +0300

    _puts.c

--- _puts.c
+++ _puts.c
@@ -2,14 +2,15 @@
 
 /**
  * _puts - prints a string
- * @s: a string
+ * @H: argument
  *
  * Return: returns the number printed
  */
 
-int _puts(char *s)
+int _puts(va_list H)
 {
 	int counter = 0;
+	char *s = va_arg(H, char *);
 
 	while (*s)
 	{

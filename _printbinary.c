2044c7fc260d53de5190050a17ec0bed37e6e6cf
Hotty Frank <hottensiamwihaki@gmail.com>
2023-04-25 21:03:12 +0300

    _printbinary.c

--- _printbinary.c
+++ _printbinary.c
@@ -1,23 +1,30 @@
 #include "main.h"
+
 /**
- * _printbinary - coverts unsigned integer to binary
- * @x: unsigned interger parameter.
- * Return: Binary value.
+ * _printbinary - converts an unsigned int into hexadecimal number
+ * @H: arguments.
+ *
+ * Return: returns the number of characters printed
  */
-int _printbinary(unsigned int x)
+
+int _printbinary(va_list H)
 {
-	unsigned int num;
-	unsigned int modul;
-	unsigned int quontient;
+	char bin[12];
+	int i = 0;
+	int counter;
+	int n;
+	unsigned int b = va_arg(H, unsigned int);
 
-	if (x <= 0)
+	while (b)
 	{
-		return (0);
+		n = b % 2;
+		bin[i] = '0' + n;
+		b /= 2;
+		i++;
 	}
-	modul = x % 2;
-	quontient = x / 2;
-
-	num = _printbinary(quontient);
-	_putchar('0' + modul);
-	return (num + 1);
+	while (i--)
+	{
+		counter += _putchar(bin[i]);
+	}
+	return (counter);
 }

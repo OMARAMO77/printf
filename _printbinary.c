8754f513a34cfaf5bce396e6b27aad49eb30701e
Omar <omar0642624692@gmail.com>
2023-04-25 13:31:45 -0700

    _printbinary.c

--- _printbinary.c
+++ _printbinary.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * _printbinary - converts an unsigned int into hexadecimal number
- * @H: arguments.
- *
- * Return: returns the number of characters printed
- */
+* _printbinary - converts an unsigned int into hexadecimal number
+* @H: arguments.
+*
+* Return: returns the number of characters printed
+*/
 
 int _printbinary(va_list H)
 {
-	char bin[12];
+	char bin[1024];
 	int i = 0;
 	int counter;
 	int n;

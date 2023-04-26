08223c8e072ea415e5d24044e19f0f9b9be6a192
Omar <omar0642624692@gmail.com>
2023-04-25 19:38:24 -0700

    _printaddr.c

---
+++ _printaddr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _printaddr - prints an hexadecimal number
+ * @H: arguments
+ * Return: counter
+ */
+int _printaddr(va_list H)
+{
+	void *ptr;
+	char *str = "(nil)";
+	long int c;
+	int b;
+	int i;
+
+	ptr = va_arg(H, void*);
+	if (ptr == NULL)
+	{
+		for (i = 0; str[i] != '\0'; i++)
+		{
+			_putchar(str[i]);
+		}
+		return (i);
+	}
+
+	c = (unsigned long int)ptr;
+	_putchar('0');
+	_putchar('x');
+	b = printhex(c);
+	return (b + 2);
+}
